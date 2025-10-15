f8db09ee1dd6e70c561a8868067f87777140738f
Gomide-Gabriel <nephilin196@gmail.com>
2025-10-15 20:02:58 -0300

    att

--- Projeto Integrador/JAVASCRIPT/NossosProdutos.js
+++ Projeto Integrador/JAVASCRIPT/NossosProdutos.js
@@ -1,51 +1,52 @@
 
 const masculineCostume = [
-    {name: 'fm1', Image: './img/FM!.png'},
-    {name: 'fm2', Image: './img/FM2.png'},
-    {name: 'fm3', Image: './img/FM3.png'},
-    {name: 'fm4', Image: './img/FM4.png'},
-    {name: 'fm5', Image: './img/FM5.png'}
-    ];
+    {name: 'fm1', image: './img/FM1.png'},
+    {name: 'fm2', image: './img/FM2.png'},
+    {name: 'fm3', image: './img/FM3.png'},
+    {name: 'fm4', image: './img/FM4.png'},
+    {name: 'fm5', image: './img/FM5.png'}
+];
 
 const feminineCostume = [
-    {name: 'ff1', Image: './img/FM!.png'},
-    {name: 'ff2', Image: './img/FM2.png'},
-    {name: 'ff3', Image: './img/FM3.png'},
-    {name: 'ff4', Image: './img/FM4.png'},
-    {name: 'ff5', Image: './img/FM5.png'}
+    {name: 'ff1', image: './img/FM!.png'},
+    {name: 'ff2', image: './img/FM2.png'},
+    {name: 'ff3', image: './img/FM3.png'},
+    {name: 'ff4', image: './img/FM4.png'},
+    {name: 'ff5', image: './img/FM5.png'}
+    
 ];
 
 const infantCostume = [
-    {name: 'fi1', Image: './img/FM!.png'},
-    {name: 'fi2', Image: './img/FM2.png'},
-    {name: 'fi3', Image: './img/FM3.png'},
-    {name: 'fi4', Image: './img/FM4.png'},
-    {name: 'fi5', Image: './img/FM5.png'}
+    {name: 'fi1', image: './img/FM!.png'},
+    {name: 'fi2', image: './img/FM2.png'},
+    {name: 'fi3', image: './img/FM3.png'},
+    {name: 'fi4', image: './img/FM4.png'},
+    {name: 'fi5', image: './img/FM5.png'}
 
 ];
 
 const mascotCostume = [
-    {name: 'fmt1', Image: './img/FM!.png'},
-    {name: 'fmt2', Image: './img/FM2.png'},
-    {name: 'fmt3', Image: './img/FM3.png'},
-    {name: 'fmt4', Image: './img/FM4.png'},
-    {name: 'fmt5', Image: './img/FM5.png'}
+    {name: 'fmt1', image: './img/FM!.png'},
+    {name: 'fmt2', image: './img/FM2.png'},
+    {name: 'fmt3', image: './img/FM3.png'},
+    {name: 'fmt4', image: './img/FM4.png'},
+    {name: 'fmt5', image: './img/FM5.png'}
 ];
 
 const comemorativeCostume = [
-    {name: 'fc1', Image: './img/FM!.png'},
-    {name: 'fc2', Image: './img/FM2.png'},
-    {name: 'fc3', Image: './img/FM3.png'},
-    {name: 'fc4', Image: './img/FM4.png'},
-    {name: 'fc5', Image: './img/FM5.png'}
+    {name: 'fc1', image: './img/FM!.png'},
+    {name: 'fc2', image: './img/FM2.png'},
+    {name: 'fc3', image: './img/FM3.png'},
+    {name: 'fc4', image: './img/FM4.png'},
+    {name: 'fc5', image: './img/FM5.png'}
 ];
 
 const acessoryCostume = [
-    {name: 'fa1', Image: './img/FM!.png'},
-    {name: 'fa2', Image: './img/FM2.png'},
-    {name: 'fa3', Image: './img/FM3.png'},
-    {name: 'fa4', Image: './img/FM4.png'},
-    {name: 'fa5', Image: './img/FM5.png'}
+    {name: 'fa1', image: './img/FM!.png'},
+    {name: 'fa2', image: './img/FM2.png'},
+    {name: 'fa3', image: './img/FM3.png'},
+    {name: 'fa4', image: './img/FM4.png'},
+    {name: 'fa5', image: './img/FM5.png'}
 ];
 
 // Objeto de animação
@@ -58,6 +59,8 @@ const animateImage = [
 // todas as listas de produto
 const myProducts = [masculineCostume, feminineCostume, infantCostume, comemorativeCostume, acessoryCostume];
 
+
+// atribui o elemento com ID carroussel
 const carrossel = document.getElementById('carrossel');
 
 // <ul>
@@ -66,9 +69,24 @@ const ulCarrossel = carrossel.children;
 // <li> 
 let liChild = [];
 
+
+// Contador para troca de imagem
+let imgCount;
+// indica se acabou a atribuição do elemento img
+let imgElementDone; 
+
+// Carrega o Script quando a tela carrega
+
 window.addEventListener('load', () => {
-    //console.log('onload')
+    imgCount = 0;
     loadImagesFunc();
+    
+    if (imgElementDone){ 
+        
+        setInterval(function() {
+            ChangeImage(liChild[0].children[0])
+            }, 2000);
+    }
 })
 
 
@@ -88,22 +106,39 @@ function loadImagesFunc(){
         imgElement.textContent = "Nova imagem";
         imgElement.width= 150;
         imgElement.height= 250;
+        imgElement.src = "";
 
-        imgElement.src = myProducts[0].at(i).Image;
-
-
+        
         if (liChild[i]) {
             
             // atribui o elemento <img> antes do <p>
             liChild[i].insertBefore(liChild[i].appendChild(imgElement), liPara[0]);
 
         }
-        else console.error("Não foi achaddo");
+        else console.error("Não foi achado");
     }
-    
+
+    imgElementDone = true;
+
+
+
 }
 
-function ChangeImage(){
+function ChangeImage(imgConteiner){
+    console.log("chamou");
+   
+    if (imgConteiner.getAttribute('src') == ""){
+        //console.log("entrou");
+        imgConteiner.src = myProducts[0].at(0).image;
+    }
+    else{
+        //console.log("segunda parte");
+        if (imgCount >= 4) imgCount = 0;
+        imgCount++;
+        imgConteiner.src  = myProducts[0].at(imgCount).image;
+        
+    }
+
     
 }
 
