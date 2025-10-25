0972ad9d4b12b4a6db437be3cccac61668e6aef7
Gomide-Gabriel <nephilin196@gmail.com>
2025-10-25 11:04:39 -0300

    att

---
+++ Design Web/AULA 12/code.js
@@ -0,0 +1,36 @@
+const input = document.getElementById('txttarefa');
+const list = document.getElementById('list')
+let count = 0;
+
+
+function adicionar(){
+    console.log(input.value);
+    count+=1;
+    const element = document.createElement('div');
+    element.innerHTML = `<span id="${count}"> ${input.value} ${count}</span>
+                        <button id="${count}" onclick="ler(${element})">
+                        feito
+                        </button>`; 
+
+
+    list.append(element);
+    
+}
+
+function ler(element){
+    console.log(element);
+}
+
+function remover(element){
+    const target = event.target;
+    target.style.display = 'none';
+    target.previousElementSibling.style.display = 'none';
+
+
+
+    const main = document.getElementById('inicio');
+    const doneList = document.createElement('div')
+    doneList.innerHTML = `<h3>Lista feita</h3> ${element}`;
+    
+    main.append(doneList);
+}
