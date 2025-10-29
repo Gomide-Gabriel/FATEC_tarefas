c3bd558f83d37eec2a77231c9b7b6ee456f7fff5
Gomide-Gabriel <nephilin196@gmail.com>
2025-10-28 21:01:18 -0300

    att

--- Projeto Integrador/JAVASCRIPT/InstantiateRandrom.js
+++ Projeto Integrador/JAVASCRIPT/InstantiateRandrom.js
@@ -1,10 +1,10 @@
 const imgUrls = [
-    {nome: 'sym1', img: './img/Symbol/sym1' }
-    {nome: 'sym2', img: './img/Symbol/sym2' }
-    {nome: 'sym3', img: './img/Symbol/sym3' }
-    {nome: 'sym4', img: './img/Symbol/sym4' }
-    {nome: 'sym5', img: './img/Symbol/sym5' }
-    {nome: 'sym6', img: './img/Symbol/sym6' }
+    {nome: 'sym1', img: './img/Symbol/sym1' },
+    {nome: 'sym2', img: './img/Symbol/sym2' },
+    {nome: 'sym3', img: './img/Symbol/sym3' },
+    {nome: 'sym4', img: './img/Symbol/sym4' },
+    {nome: 'sym5', img: './img/Symbol/sym5' },
+    {nome: 'sym6', img: './img/Symbol/sym6' },
     {nome: 'sym7', img: './img/Symbol/sym7' }
 ];
 
@@ -20,10 +20,13 @@ window.addEventListener(`load`, () => {
 function createElement(max, min){
 
     let randSize = Math.floor(Math.random() * (max-min)) + min;
+    let randPos = Math.floor(Math.random() * ((max*15) -min)) + min;
 
     const imgElement = document.createElement(`img`);
     imgElement.src = "";
     imgElement.style.height = randSize; 
     imgElement.style.width = randSize;
+    imgElement.style.top = randPos;
+    imgElement.style.bottom = randPos;
 
 }
