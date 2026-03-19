90025385502f44d8208fd61c7731d517c9a41e5a
= <=>
2026-03-19 10:47:22 -0300

    att

---
+++ .github/workflows/Python/Ordenação/ex1.py
@@ -0,0 +1,10 @@
+list = [8,4,5,2,1,0,9,6]
+n = len(list)
+
+for i in len(n):
+    maior = i
+    for x in range(maior+1, n):
+        if x > maior:
+            novo_maior = x
+            maior = 
+
